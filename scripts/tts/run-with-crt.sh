ec108e54cb757afffc5d01ac3a44655f07a562fb
Dmitrii <zeionara@gmail.com>
2020-10-23 20:29:57 +0300

    Updated script for using crt

--- scripts/tts/run-with-crt.sh
+++ scripts/tts/run-with-crt.sh
@@ -1,4 +1,10 @@
-for file in crt-data/*; do
-	export audio_path=crt-audios/$(echo $file | cut -d '/' -f2).wav
-	python crt.py -f $file -o $audio_path
+export input_dir=${1:-assets/baneks}
+let i=0
+let total=$(ls -alh $input_dir | wc -l)
+let total=total-3
+for file in $input_dir/*; do
+	export audio_path=audios/$(echo $file | cut -d '/' -f3 | cut -d '.' -f1).wav
+	python -m suitable-hat tts crt --input-file $file --output-file $audio_path --after-chunk-delay 2 --after-file-delay 4 --max-n-chars 500
+  let i=i+1
+  echo Handled $i/$total files
 done
